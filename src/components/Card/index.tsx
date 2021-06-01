99b262f8af4e458281935bf4193a7d58e11e8801
Miguel Nogueira <mglsndc@gmail.com>
2021-06-01 17:30:44 -0300

    extend html div element attributes on card

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -2,25 +2,27 @@ import { Wrapper, Overlay, Details, Rating } from './styles'
 
 import { RiStarFill } from 'react-icons/ri'
 
-interface CardProps {
+interface CardProps extends React.HtmlHTMLAttributes<HTMLDivElement> {
   name: string
   popularity: number
   backdropPath?: string
 }
 
-export function Card({ name, popularity, backdropPath }: CardProps) {
+export function Card({ name, popularity, backdropPath, ...rest }: CardProps) {
   return (
-    <Wrapper
-      backdropPath={`https://image.tmdb.org/t/p/original${backdropPath}`}
-    >
-      <Overlay />
-      <Details>
-        <span>{name}</span>
-        <Rating>
-          <h5>{popularity}</h5>
-          <RiStarFill color="#F6C800" />
-        </Rating>
-      </Details>
-    </Wrapper>
+    <div {...rest}>
+      <Wrapper
+        backdropPath={`https://image.tmdb.org/t/p/original${backdropPath}`}
+      >
+        <Overlay />
+        <Details>
+          <span>{name}</span>
+          <Rating>
+            <h5>{popularity}</h5>
+            <RiStarFill color="#F6C800" />
+          </Rating>
+        </Details>
+      </Wrapper>
+    </div>
   )
 }
