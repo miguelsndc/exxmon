6d91f0c3919e414c42fd4da2502dfcf925a6f96c
Miguel Nogueira <mglsndc@gmail.com>
2021-06-02 16:16:28 -0300

    remove right sidebar and add movie provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,20 +3,22 @@ import { ThemeProvider } from 'styled-components'
 import { GlobalStyles } from '../styles/globals'
 
 import { LeftSidebar } from '../components/LeftSidebar'
-import { RightSidebar } from '../components/RightSidebar'
 
 import { DarkTheme } from '../styles/themes/dark'
+import { MovieProvider } from '../contexts/MovieContext'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ThemeProvider theme={DarkTheme}>
-      <GlobalStyles />
-      <main style={{ display: 'flex' }}>
-        <LeftSidebar />
-        <Component {...pageProps} />
-        {/* <RightSidebar /> */}
-      </main>
-    </ThemeProvider>
+    <MovieProvider>
+      <ThemeProvider theme={DarkTheme}>
+        <GlobalStyles />
+        <main style={{ display: 'flex' }}>
+          <LeftSidebar />
+          <Component {...pageProps} />
+          {/* <RightSidebar /> */}
+        </main>
+      </ThemeProvider>
+    </MovieProvider>
   )
 }
 
