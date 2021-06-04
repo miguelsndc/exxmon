79e6200b104dec43eb07f84fcfada61fda02073e
Miguel Nogueira <mglsndc@gmail.com>
2021-06-04 16:17:50 -0300

    create discover page initial structure

---
+++ src/styles/pages/Discover.ts
@@ -0,0 +1,31 @@
+import styled from 'styled-components'
+
+export const SearchForm = styled.form`
+  margin-top: 2rem;
+
+  input[type='text'] {
+    border: 2px solid ${(props) => props.theme.text};
+    outline: 0;
+    padding: 0.5rem 1rem;
+    border-radius: 10px;
+    font-weight: 500;
+
+    font: inherit;
+    color: white;
+
+    background: transparent;
+    margin-right: 1rem;
+  }
+
+  button[type='submit'] {
+    border: 0;
+    outline: 0;
+    padding: 0.65rem 1rem;
+    border-radius: 10px;
+
+    background: ${(props) => props.theme.red400};
+    font: inherit;
+    color: white;
+    font-weight: 500;
+  }
+`
