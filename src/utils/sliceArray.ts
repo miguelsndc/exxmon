50c799fbd2aa651749b359c1338baec6be3a9d3f
Miguel Nogueira <mglsndc@gmail.com>
2021-06-05 20:36:29 -0300

    remove slicearray func

--- src/utils/sliceArray.ts
+++
@@ -1,9 +0,0 @@
-export function sliceArray<T>(
-  arr: T[] | undefined,
-  beginning: number,
-  end: number
-) {
-  if (!arr || arr == null) return undefined
-
-  return arr?.slice(beginning, end)
-}
