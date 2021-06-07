d2beb14adae6a9ae9f19c78ee4ec095d871e2b84
Miguel Nogueira <mglsndc@gmail.com>
2021-06-07 18:16:17 -0300

    add path prop & add movie title max char length

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -3,25 +3,36 @@ import { Wrapper, Overlay, Details, Rating } from './styles'
 import { RiStarFill } from 'react-icons/ri'
 
 import Image from 'next/image'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 interface CardProps extends React.HtmlHTMLAttributes<HTMLDivElement> {
   name: string
   popularity: number
   backdropPath?: string
+  path?: string
 }
 
-export function Card({ name, popularity, backdropPath, ...rest }: CardProps) {
+export function Card({
+  name,
+  popularity,
+  backdropPath,
+  path,
+  ...rest
+}: CardProps) {
+  const router = useRouter()
+
   const myLoader = ({ src }) => {
     return `${src}`
   }
 
   return (
-    <div {...rest}>
+    <div {...rest} onClick={() => router.push(path)}>
       <Wrapper hasPoster={!!backdropPath}>
         <Image loader={myLoader} layout="fill" src={`${backdropPath}`} />
         <Overlay />
         <Details>
-          <span>{name}</span>
+          <span>{name.length > 25 ? `${name.slice(0, 20)}...` : name}</span>
           <Rating>
             <h5>{popularity}</h5>
             <RiStarFill color="#F6C800" />
