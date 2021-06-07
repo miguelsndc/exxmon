b975c68c3ba608937088521a0079861e9c5bca79
Miguel Nogueira <mglsndc@gmail.com>
2021-06-07 18:24:09 -0300

    change movie details layout

--- src/pages/movies/[id].tsx
+++ src/pages/movies/[id].tsx
@@ -7,31 +7,24 @@ import { useRouter } from 'next/router'
 import { HorizontalScrollSection } from '../../components/HorizontalScroll'
 import { Loading } from '../../components/Loading'
 import { Card } from '../../components/Card'
+import { FeaturedMovie } from '../../components/FeaturedMovie'
 
 import { Genre, MovieDetails, MovieResponse } from '../../types/Movie'
 
 import { api } from '../../services/api'
 
 import { CtaButton } from '../../styles/pages/Popular'
-import {
-  MoviePoster,
-  Overlay,
-  MovieInfo,
-  Details,
-  ResultsNotFound,
-  AdditionalInfo,
-} from '../../styles/pages/Id'
+import { MoviePoster, ResultsNotFound } from '../../styles/pages/Id'
 
-import Image from 'next/image'
 import Link from 'next/link'
 
-interface SimilarMovie {
+type SimilarMovie = {
   id: string
   posterPath: string
   title: string
   rating: number
 }
-interface MovieDetailsProps {
+type MovieDetailsProps = {
   releaseDate: string
   backdropPath: string
   posterPath: string
@@ -42,7 +35,7 @@ interface MovieDetailsProps {
   status: string
 }
 
-interface Params {
+type Params = {
   movieDetails: MovieDetailsProps
   similarMovies: SimilarMovie[]
 }
@@ -57,83 +50,26 @@ export default function SpecificMovieDetails({
     return <Loading />
   }
 
-  function formatDate(date?: string) {
-    if (!date || date == null) return undefined
-
-    const currentDate = format(new Date(date), 'MMM/dd/yyyy', { locale: enUS })
-
-    return currentDate
-  }
-
-  const formattedDate = formatDate(movieDetails?.releaseDate)
-
-  const backgroundLoader = ({ src }) => {
-    return `https://image.tmdb.org/t/p/original${src}`
-  }
-
-  const posterLoader = ({ src }) => {
-    return `https://image.tmdb.org/t/p/w500${src}`
-  }
-
   return (
     <section>
       <MoviePoster>
-        <Image
-          loader={backgroundLoader}
-          src={`https://image.tmdb.org/t/p/w500/${movieDetails?.backdropPath}`}
-          layout="fill"
-          objectFit="cover"
+        <FeaturedMovie
+          backdropPath={movieDetails.backdropPath}
+          overview={movieDetails.overview}
+          genres={movieDetails.genres}
+          originalTitle={movieDetails.title}
+          releaseDate={movieDetails.releaseDate}
+          rating={movieDetails.rating}
         />
-        <Overlay />
-        <MovieInfo>
-          <div style={{ width: '200px', height: '300px' }}>
-            <Image
-              loader={posterLoader}
-              src={`https://image.tmdb.org/t/p/w500/${movieDetails?.posterPath}`}
-              width={200}
-              height={300}
-              layout="fixed"
-            />
-          </div>
-
-          <Details>
-            <h2>{movieDetails?.title || movieDetails?.title}</h2>
-            <p>{movieDetails?.overview}</p>
-            <div>
-              <AdditionalInfo>
-                <img src="/assets/images/imdb.svg" alt="IMDB" />
-                <h2>{movieDetails?.rating}</h2>
-              </AdditionalInfo>
-              <AdditionalInfo>
-                <h3>
-                  Genres:{' '}
-                  {movieDetails?.genres.map((genre, index) => {
-                    return <span key={index}>{genre.name},</span>
-                  })}
-                </h3>
-              </AdditionalInfo>
-              <AdditionalInfo>
-                <h3>
-                  Release date: <span>{formattedDate}</span>
-                </h3>
-              </AdditionalInfo>
-              <AdditionalInfo>
-                <h3>
-                  Status: <span>{movieDetails?.status}</span>
-                </h3>
-              </AdditionalInfo>
-            </div>
-          </Details>
-        </MovieInfo>
       </MoviePoster>
 
       {similarMovies.length ? (
-        <HorizontalScrollSection title="Similar Movies">
+        <HorizontalScrollSection title="Similar Movies" path="/movies/popular">
           {similarMovies?.map((movie) => {
             return (
               <Card
                 key={movie.id}
-                onClick={() => router.push(`/movies/${movie.id}`)}
+                path={`/movies/${movie.id}`}
                 name={movie.title}
                 popularity={movie.rating}
                 backdropPath={`https://image.tmdb.org/t/p/w500/${movie.posterPath}`}
@@ -172,10 +108,8 @@ export const getStaticPaths: GetStaticPaths = async () => {
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
   const { id } = params
+
   const movieDetailsResponse = await api.get<MovieDetails>(`/movie/${id}`)
-  const similarMoviesResponse = await api.get<MovieResponse>(
-    `/movie/${id}/similar`
-  )
 
   const movieDetails = {
     releaseDate: movieDetailsResponse.data.release_date,
@@ -189,6 +123,12 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
     genres: movieDetailsResponse.data.genres,
     status: movieDetailsResponse.data.status,
   }
+
+  console.log(movieDetails)
+
+  const similarMoviesResponse = await api.get<MovieResponse>(
+    `/movie/${id}/similar`
+  )
 
   const similarMovies = similarMoviesResponse.data.results.map((movie) => {
     return {
