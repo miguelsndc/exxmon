d635c3d22e45115446eae353c7ce76722c257516
Miguel Nogueira <mglsndc@gmail.com>
2021-06-07 18:25:32 -0300

    change fonts

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -41,7 +41,7 @@ export default class MyDocument extends Document {
         <Head>
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
-            href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Inter:wght@300;400;500;600;700&display=swap"
             rel="stylesheet"
           />
         </Head>
