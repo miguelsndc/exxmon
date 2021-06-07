83b80ca7505c072e3cf656122bb952ed3e78bf76
Miguel Nogueira <mglsndc@gmail.com>
2021-06-07 18:19:11 -0300

    decrase grid container width and decrease row gap

--- src/styles/pages/Popular.ts
+++ src/styles/pages/Popular.ts
@@ -1,19 +1,28 @@
 import styled from 'styled-components'
 
 export const MostPopularSection = styled.section`
+  margin-top: 8vh;
   h1 {
     margin-bottom: 2rem;
   }
+
+  display: grid;
+  place-items: center;
 `
 
 export const GridContainer = styled.div`
   display: grid;
   row-gap: 2.5rem;
+  width: 75%;
+  column-gap: 0.5rem;
   justify-content: center;
+
   grid-template-columns: repeat(5, 1fr);
+
   @media (max-width: 1544px) {
     grid-template-columns: repeat(4, 1fr);
   }
+
   @media (max-width: 1080px) {
     grid-template-columns: repeat(3, 1fr);
   }
@@ -32,7 +41,7 @@ const Button = styled.button`
 `
 
 export const CtaButton = styled(Button)`
-  background: ${(props) => props.theme.red400};
+  background: ${(props) => props.theme.primary};
 
   margin-top: 2rem;
   padding: 1rem 2.25rem;
