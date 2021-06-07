b413b616241c05003b068a211d81f715ab4ebcf2
Miguel Nogueira <mglsndc@gmail.com>
2021-06-07 18:20:43 -0300

    put menu fixed on top of the page full width

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,51 +1,69 @@
-import { Wrapper, Logo, MenuTitle, MenuItem } from './styles'
+import { Wrapper, Logo, MenuItem, NavigationLinks, SearchForm } from './styles'
 
 import Link from 'next/link'
 
-import {
-  RiMovie2Line,
-  RiHomeFill,
-  RiCompass4Fill,
-  RiSearchLine,
-} from 'react-icons/ri'
+import { RiMovie2Line } from 'react-icons/ri'
+import { useForm } from 'react-hook-form'
+import { DarkTheme } from '../../styles/themes/dark'
+import { useEffect, useState } from 'react'
 
-import { BiTrendingUp } from 'react-icons/bi'
+type FormData = {
+  query: string
+}
 
 export function Menu() {
+  const { register, handleSubmit } = useForm<FormData>()
+  const [hasScrolled, setHasScrolled] = useState(false)
+
+  function searchMovie(data: FormData) {
+    console.log(data)
+  }
+
+  function scroll() {
+    if (window.scrollY > 200) {
+      setHasScrolled(true)
+    } else {
+      setHasScrolled(false)
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', scroll)
+
+    return () => {
+      window.removeEventListener('scroll', scroll)
+    }
+  }, [])
+
   return (
-    <Wrapper>
-      <Link href="/">
-        <Logo>
-          <RiMovie2Line color={'#d22f27'} size="2rem" />
-          <span>Exxmon</span>
-        </Logo>
-      </Link>
-      <MenuTitle>menu</MenuTitle>
+    <Wrapper hasScrolled={hasScrolled}>
       <nav>
         <Link href="/">
-          <MenuItem>
-            <RiHomeFill color={'#5c5d63'} size="1.5rem" />
-            <span>Home</span>
-          </MenuItem>
+          <Logo>
+            <RiMovie2Line color={DarkTheme.primary} size="2rem" />
+            <span>Exxmon</span>
+          </Logo>
         </Link>
-        <Link href="/movies/search">
-          <MenuItem>
-            <RiSearchLine color={'#5c5d63'} size="1.5rem" />
-            <span>Search</span>
-          </MenuItem>
-        </Link>
-        <Link href="/movies/popular">
-          <MenuItem>
-            <BiTrendingUp color={'#5c5d63'} size="1.5rem" />
-            <span>Popular</span>
-          </MenuItem>
-        </Link>
-        <Link href="/movies/discover">
-          <MenuItem>
-            <RiCompass4Fill color={'#5c5d63'} size="1.5rem" />
-            <span>Discovery</span>
-          </MenuItem>
-        </Link>
+        <NavigationLinks>
+          <div>
+            <Link href="/">
+              <MenuItem>Home</MenuItem>
+            </Link>
+            <Link href="/movies/popular">
+              <MenuItem>Popular</MenuItem>
+            </Link>
+            <Link href="/movies/discover">
+              <MenuItem>Discovery</MenuItem>
+            </Link>
+          </div>
+          <SearchForm onSubmit={handleSubmit(searchMovie)}>
+            <input
+              type="text"
+              placeholder="Search"
+              {...register('query', { required: true })}
+            />
+          </SearchForm>
+        </NavigationLinks>
       </nav>
     </Wrapper>
   )
