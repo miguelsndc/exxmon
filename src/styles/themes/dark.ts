c40be904c444b99346513bb41fda19363aa14a95
Miguel Nogueira <mglsndc@gmail.com>
2021-06-08 17:05:51 -0300

    change primary color

--- src/styles/themes/dark.ts
+++ src/styles/themes/dark.ts
@@ -2,7 +2,7 @@ export const DarkTheme = {
   background: '#101012',
   white: '#fff',
   gray400: '#2d2d35',
-  primary: '#f31534',
+  primary: '#F6C800',
   text: '#5c5d63',
   title: '#9696a3',
 }
