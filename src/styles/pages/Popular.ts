14bb5ba1f31f99545120b7f4958850c008acdd51
Miguel Nogueira <mglsndc@gmail.com>
2021-06-08 10:20:18 -0300

    change popular section title font

--- src/styles/pages/Popular.ts
+++ src/styles/pages/Popular.ts
@@ -2,8 +2,11 @@ import styled from 'styled-components'
 
 export const MostPopularSection = styled.section`
   margin-top: 8vh;
+
   h1 {
-    margin-bottom: 2rem;
+    margin-bottom: 1rem;
+    font-size: 4rem;
+    font-family: 'Bebas Neue', cursive;
   }
 
   display: grid;
