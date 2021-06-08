0b1ca67f2134e85ecea1abca64f6655820d57184
Miguel Nogueira <mglsndc@gmail.com>
2021-06-08 17:02:31 -0300

    make responsive

--- src/components/FeaturedMovie/styles.ts
+++ src/components/FeaturedMovie/styles.ts
@@ -1,39 +1,76 @@
 import styled from 'styled-components'
 
-export const Poster = styled.div`
+type PosterProps = {
+  hasPoster: boolean
+}
+
+export const Poster = styled.div<PosterProps>`
   width: 100%;
   position: relative;
 
+  background: ${(props) => props.hasPoster && 'black'};
+
   display: flex;
   align-items: center;
-  height: 85vh;
+  height: 95vh;
 
   @media (max-width: 600px) {
     text-align: center;
     justify-content: center;
   }
 `
-export const MovieDetails = styled.div`
-  padding-left: 3rem;
 
+export const PosterFallback = styled.div`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  background: black;
   z-index: 2;
 
-  h1,
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
   h2 {
+    font-size: 2rem;
+    text-transform: capitalize;
+    filter: brightness(0.8);
+  }
+`
+
+export const MovieDetails = styled.div`
+  padding-left: 3rem;
+  margin-top: 1rem;
+
+  z-index: 9;
+
+  h1,
+  h2,
+  h3 {
     font-family: 'Bebas Neue', -apple-system, BlinkMacSystemFont, 'Segoe UI',
       Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue',
       sans-serif;
     text-transform: uppercase;
-    line-height: 0.8;
+    line-height: 0.9;
   }
 
   h1 {
-    font-size: 5.25rem;
+    letter-spacing: 1px;
+    font-size: 5rem;
+    margin-left: -1.4rem;
   }
 
   h2 {
-    font-size: 4.5rem;
-    margin-bottom: 1.25rem;
+    font-size: 4.25rem;
+    color: ${(props) => props.theme.primary};
+    letter-spacing: 1px;
+    margin: 1rem 0;
+  }
+
+  h3 {
+    margin-bottom: 1rem;
+    font-size: 2rem;
+    color: ${(props) => props.theme.primary};
   }
 
   span {
@@ -41,6 +78,16 @@ export const MovieDetails = styled.div`
     font-weight: 500;
     font-size: 1.2rem;
     color: white;
+  }
+
+  @media (max-width: 768px) {
+    h1 {
+      font-size: 3.25rem;
+      margin-left: 0;
+    }
+    h2 {
+      font-size: 2.5rem;
+    }
   }
 
   @media (max-width: 600px) {
@@ -101,4 +148,10 @@ export const Description = styled.p`
   @media (max-width: 700px) {
     width: 90%;
   }
+`
+
+export const Tagline = styled.h3`
+  font-size: 1.85rem;
+  margin-top: 1rem;
+  font-style: italic;
 `
